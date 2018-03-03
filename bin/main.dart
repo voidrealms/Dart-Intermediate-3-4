daf84cf5283046e57e6462f684300dc4cbcc61fd
Rootshell <bcairns@voidrealms.com>
2018-03-03 11:38:36 -0500

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:scope4/animal.dart';
+
+main(List<String> arguments) {
+
+  Animal cat = new Animal();
+  Animal dog = new Animal();
+  Animal bird = new Animal();
+  Animal fish = new Animal();
+
+  Animal.run();
+
+}
---
+++ test/scope4_test.dart
@@ -0,0 +1,8 @@
+import 'package:scope4/scope4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
